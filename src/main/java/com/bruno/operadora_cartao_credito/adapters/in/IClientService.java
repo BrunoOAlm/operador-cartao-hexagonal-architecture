396e7bcc8e20463a37f794c1e3974fc054217506
Bruno Almeida <97802070+BrunoOAlm@users.noreply.github.com>
2026-04-14 16:30:12 -0300

    Revert "Feature/controller"

--- src/main/java/com/bruno/operadora_cartao_credito/adapters/in/IClientService.java
+++
@@ -1,9 +0,0 @@
-package com.bruno.operadora_cartao_credito.adapters.in;
-
-import com.bruno.operadora_cartao_credito.application.domain.ClienteDomain;
-
-public interface IClientService {
-
-    ClienteDomain solicitarCartao(ClienteDomain cliente);
-    ClienteDomain buscarPorCpf(String cpf);
-}
